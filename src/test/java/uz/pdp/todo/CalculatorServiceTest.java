df41294f8513af8cbae7c4e1cd5327f5a110a6df
muhammad <olmospro1978@gmail.com>
2025-12-02 17:44:03 +0500

    unit test

--- src/test/java/uz/pdp/todo/CalculatorServiceTest.java
+++ src/test/java/uz/pdp/todo/CalculatorServiceTest.java
@@ -1,6 +1,7 @@
 package uz.pdp.todo;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -8,11 +9,13 @@ import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayNameGeneration(value = DisplayNameGenerator.IndicativeSentences.class)
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 class CalculatorServiceTest {
 
     private CalculatorService calculatorService;
+
 
     @BeforeEach
     void setUp() {
@@ -21,6 +24,7 @@ class CalculatorServiceTest {
 
     }
 
+    @Disabled
     @Test
     void add() {
         double a = 1.1;
@@ -30,12 +34,16 @@ class CalculatorServiceTest {
         assertEquals(expect, result);
     }
 
-    @Test
+    @RepeatedTest(value = 10)
+//    @EnabledOnOs(value = {OS.WINDOWS, OS.MAC})
+//    @EnabledOnJre(value = JRE.JAVA_8)
+//    @EnabledForJreRange(min = JRE.JAVA_17, max = JRE.JAVA_18)
     void should_happen_error_when_b_zero() {
         double a = 1.1;
         double b = 0;
         ArithmeticException exception = assertThrows(ArithmeticException.class, () -> calculatorService.divide(a, b));
         assertEquals("b 0 bo'lmasligi kerak", exception.getMessage());
+        System.out.println("should_happen_error_when_b_zero ishladi");
     }
 
     @Test

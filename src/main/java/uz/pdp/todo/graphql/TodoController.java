c8134e7d05d58c0fe1d3f9361668da85b6402511
muhammad <olmospro1978@gmail.com>
2025-12-06 17:45:17 +0500

    graphql test

--- src/main/java/uz/pdp/todo/graphql/TodoController.java
+++ src/main/java/uz/pdp/todo/graphql/TodoController.java
@@ -2,6 +2,8 @@ package uz.pdp.todo.graphql;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,23 +20,26 @@ public class TodoController {
 
     private final TodoService service;
 
-    @SchemaMapping(typeName = "Query", value = "getTodos")
+    //    @SchemaMapping(typeName = "Query", value = "getTodos") // requestmapping  // getmapping
+    @QueryMapping("getTodos")
     public List<TodoDto> getAll() {
         return service.getAll();
     }
 
 
-    @SchemaMapping(typeName = "Query", value = "getTodo")
+    //    @SchemaMapping(typeName = "Query", value = "getTodo")
+    @QueryMapping("getTodo")
     public TodoDto get(@Argument Long id) {
         return service.get(id);
     }
 
-    @SchemaMapping(typeName = "Mutation", value = "createTodo")
+    @MutationMapping("createTodo")
     public TodoDto get(@Argument TodoCreateDto dto) {
         return service.create(dto);
     }
 
-    @SchemaMapping(typeName = "Mutation", value = "deleteTodo")
+    @MutationMapping("deleteTodo")
+//    @SchemaMapping(typeName = "Mutation", value = "deleteTodo")
     public Boolean delete(@Argument Long id) {
         service.delete(id);
         return true;
